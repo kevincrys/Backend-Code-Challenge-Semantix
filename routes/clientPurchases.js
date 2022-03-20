7f279a694ac7d80e81f62273976d60af42dd76d1
kevincrys <54685815+kevincrys@users.noreply.github.com>
2022-03-20 19:07:32 -0300

    Update clientPurchases.js

--- routes/clientPurchases.js
+++ routes/clientPurchases.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 const dbsql = require("../db/dbsql");
 
+
+//operação de listagem de compra por cliente
 router.get('/:id', function(req, res, next) {
   var purchases= dbsql.selectPurchasesByClient(req.params.id)
     purchases.then(sql => {res.send(sql)})
