d7aa7723b66528306892916aa2c14d73c761507e
kevincrys <54685815+kevincrys@users.noreply.github.com>
2022-03-20 18:59:32 -0300

    Update purchases.test.js

--- purchases.test.js
+++ purchases.test.js
@@ -4,6 +4,6 @@ const dbsql = require("./db/dbsql");
   const  purchases = await dbsql.selectPurchases();
     expect(purchases.length).toBeGreaterThan(0)
 
-
+//testando se o resultado não é um array vazio
 
 });
