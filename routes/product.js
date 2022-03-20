eed9c38143ae18e1326af6be65d59580a7869f46
kevincrys <54685815+kevincrys@users.noreply.github.com>
2022-03-20 19:05:44 -0300

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -21,7 +21,7 @@ router.post('/:name/:quantity/:price', (req, res) => {
 
     });
 
-//operação de atualização dos dados de produtos cadastrados
+//operação de atualização de dados de produtos cadastrados
   router.put('/:name/:quantity/:price', (req, res) => {
     var client = dbsql.updateProductById({name: req.params.name,quantity: req.params.quantity,price: req.params.price});
       client.then(sql => {res.send(sql)})
@@ -31,7 +31,7 @@ router.post('/:name/:quantity/:price', (req, res) => {
 
       });
 
-//operação de deleção dos dados de produtos cadastrados
+//operação de deleção de dados de produtos cadastrados
   router.delete('/:name', (req, res) => {
     var client = dbsql.deleteProductById(req.params.name);
       client.then(sql => {res.send(sql)})
