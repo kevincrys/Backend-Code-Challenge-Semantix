d484f4922f9b963cb275fb4ee9351d21a21f22f8
kevincrys <54685815+kevincrys@users.noreply.github.com>
2022-03-20 18:59:16 -0300

    Update product.test.js

--- product.test.js
+++ product.test.js
@@ -3,7 +3,7 @@ const dbsql = require("./db/dbsql");
 
   const product = await dbsql.selectProduct();
     expect(product.length).toBeGreaterThan(0)
-
+//testando se o resultado não é um array vazio
 
 
 });
