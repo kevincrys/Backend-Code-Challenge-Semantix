49d77ccdf689797b68f8a14b5291849f86e71973
kevincrys <54685815+kevincrys@users.noreply.github.com>
2022-03-20 19:08:13 -0300

    Update mostPurchasedProduct.js

--- routes/mostPurchasedProduct.js
+++ routes/mostPurchasedProduct.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const dbsql = require("../db/dbsql");
 
+//operação de listagem de produtos mais vendidos em data determinada
 router.get('/:type/:date', function(req, res, next) {
   var purchases= dbsql.selectPurchasesByCountandDate(req.params.type,req.params.date)
     purchases.then(sql => {res.send(sql)})
