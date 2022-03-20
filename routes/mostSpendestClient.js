ebf6cc50948c97c2193abb604c85be4d4ddff7c5
kevincrys <54685815+kevincrys@users.noreply.github.com>
2022-03-20 19:08:36 -0300

    Update mostSpendestClient.js

--- routes/mostSpendestClient.js
+++ routes/mostSpendestClient.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const dbsql = require("../db/dbsql");
 
+//operação de listagem de clientes que mais compram em data determinada
 router.get('/:type/:date', function(req, res, next) {
   var purchases= dbsql.selectClientBySpentandDate(req.params.type,req.params.date)
    purchases.then(sql => {res.send(sql)})
