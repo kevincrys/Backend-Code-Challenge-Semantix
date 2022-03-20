38d9804214077676407bfa64ad93d541966d90c1
kevincrys <54685815+kevincrys@users.noreply.github.com>
2022-03-20 19:04:11 -0300

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const dbsql = require("../db/dbsql");
 
-//operação de leitura dos dados de usuários cadastrados
+//operação de leitura dos dados de produtos cadastrados
 router.get('/', function(req, res, next) {
   var product = dbsql.selectProduct();
    product.then(sql => {res.send(sql)})
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
     })
 })
 
-//operação de criaçao de dados de usuários cadastrados
+//operação de criaçao de dados de produtos cadastrados
 router.post('/:name/:quantity/:price', (req, res) => {
   var client = dbsql.insertProduct({name: req.params.name,quantity: req.params.quantity,price: req.params.price});
   client.then(sql => {res.send(sql)})
@@ -21,7 +21,7 @@ router.post('/:name/:quantity/:price', (req, res) => {
 
     });
 
-//operação de atualização dos dados de usuários cadastrados
+//operação de atualização dos dados de produtos cadastrados
   router.put('/:name/:quantity/:price', (req, res) => {
     var client = dbsql.updateProductById({name: req.params.name,quantity: req.params.quantity,price: req.params.price});
       client.then(sql => {res.send(sql)})
@@ -31,7 +31,7 @@ router.post('/:name/:quantity/:price', (req, res) => {
 
       });
 
-//operação de deleção dos dados de usuários cadastrados
+//operação de deleção dos dados de produtos cadastrados
   router.delete('/:name', (req, res) => {
     var client = dbsql.deleteProductById(req.params.name);
       client.then(sql => {res.send(sql)})
