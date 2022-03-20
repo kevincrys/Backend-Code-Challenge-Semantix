f27d6a672f88fad9ad7e28b95bd8552d106cca68
kevincrys <54685815+kevincrys@users.noreply.github.com>
2022-03-20 19:07:00 -0300

    Update purchases.js

--- routes/purchases.js
+++ routes/purchases.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const dbsql = require("../db/dbsql");
 
+//operação de leitura dos dados de compras cadastradas
 router.get('/', function(req, res, next) {
   var purchases = dbsql.selectPurchases();
    purchases.then(sql => {res.send(sql)})
@@ -10,6 +11,7 @@ router.get('/', function(req, res, next) {
     })
 })
 
+//operação de criação de dados de compras 
 router.post('/:idclient/:product/:date', (req, res) => {
 
   var client = dbsql.insertPurchases({idclient: req.params.idclient, product: req.params.product, date: req.params.date});
@@ -20,6 +22,7 @@ router.post('/:idclient/:product/:date', (req, res) => {
 
     });
 
+//operação de atualização de dados de compras cadastradas
   router.put('/:id/:idclient/:product/:date', (req, res) => {
     var client = dbsql.updatePurchasesById(req.params.id,{idclient: req.params.idclient,product: req.params.product,date: req.params.date});
       client.then(sql => {res.send(sql)})
@@ -29,6 +32,8 @@ router.post('/:idclient/:product/:date', (req, res) => {
 
       });
       
+
+//operação de deleção de dados de compras cadastradas
   router.delete('/:id', (req, res) => {
     var client = dbsql.deletePurchasesById(req.params.id);
       client.then(sql => {res.send(sql)})
