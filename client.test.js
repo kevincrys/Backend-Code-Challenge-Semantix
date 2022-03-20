3ef9fe739b29803977b6d7fa13a038562a24289a
kevincrys <54685815+kevincrys@users.noreply.github.com>
2022-03-20 18:58:59 -0300

    Update client.test.js

--- client.test.js
+++ client.test.js
@@ -3,7 +3,7 @@ const dbsql = require("./db/dbsql");
 
   const  client = await dbsql.selectClient();
     expect(client.length).toBeGreaterThan(0)
-
+//testando se o resultado não é um array vazio
 
 
 });
