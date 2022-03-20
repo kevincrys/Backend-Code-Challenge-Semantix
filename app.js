f728529a0a9be1934662126b1bbeba18fa218e52
kevincrys <kevin.c.costa@edu.unirio.br>
2022-03-19 23:53:30 -0300

    evolll

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var dbsql = require('./db/dbsql');
 //var client = dbsql.selectClient();
 //  client.then(sql => {console.log(sql)})
 
-var product = dbsql.selectProduct();
- product.then(sql => {console.log(sql)})
+//var product = dbsql.selectProduct();
+ //product.then(sql => {console.log(sql)})
 
 //var Purchases = dbsql.selectPurchases();
  //Purchases.then(sql => {console.log(sql)})
@@ -17,9 +17,9 @@ var product = dbsql.selectProduct();
 //var product = dbsql.insertProduct({name:'calça' ,quantity:50 ,price:80.00})
 //  product.then(sql => {console.log(sql)})
 
-//var Purchases = dbsql.insertPurchases({})
-//  Purchases.then(sql => {console.log(sql)})
-
-
-
+//var purchases = dbsql.insertPurchases({idclient:1,product:'bermuda' ,date:'2022-03-17'})
+ //purchases.then(sql => {console.log(sql)})
+//'month','2022-03-17'
+var purchases= dbsql.selectPurchasesByClientandDate(4,'month','2022-03-17')
+  purchases.then(sql => {console.log(sql)})
 app.listen(3000, function(){});
--- db/dbsql.js
+++ db/dbsql.js
@@ -45,27 +45,27 @@ async function insertProduct(customer){
 }
 async function insertPurchases(customer){
     const conn = await connect();
-    const sql = 'INSERT INTO client(name,tel,email) VALUES ($1,$2,$3);';
-    const values = [customer.idClient, customer.product, customer.price, customer.date];
+    const sql = 'INSERT INTO Purchases(idclient,product,date) VALUES ($1,$2,$3);';
+    const values = [customer.idclient, customer.product, customer.date];
     return await conn.query(sql, values);
 }
 
 
 async function updateClientById(id, customer){
     const conn = await connect();
-    const sql = 'UPDATE Client SET name=?,tel=?,email=? WHERE Client.id=?';
+    const sql = 'UPDATE Client SET name=$1,tel=$2,email=$3 WHERE Client.id=$4';
     const values = [ customer.name, customer.tel, customer.email,id];
     return await conn.query(sql, values);
 }
 async function updateProductById(id, customer){
     const conn = await connect();
-    const sql = 'UPDATE Product SET name=?,quantity=?,price=? WHERE Product.name=?';
+    const sql = 'UPDATE Product SET name=$1,quantity=$2,price=$3 WHERE Product.name=$4';
     const values = [ customer.name, customer.quantity, customer.price,id];
     return await conn.query(sql, values);
 }
 async function updatePurchasesById(id, customer){
     const conn = await connect();
-    const sql = 'UPDATE Purchases SET idClient=?,product=?,price=?,date=? WHERE Purchase.idProduct=?';
+    const sql = 'UPDATE Purchases SET idClient=$1,product=$2,price=$3,date=$4 WHERE Purchase.idProduct=$5';
     const values = [ customer.idClient, customer.product, customer.price, customer.date,id];
     return await conn.query(sql, values);
 }
@@ -73,18 +73,47 @@ async function updatePurchasesById(id, customer){
 
 async function deleteClientById(id){
       const client = await connect();
-      const sql = 'DELETE FROM Client where Client.id=?;';
+      const sql = 'DELETE FROM Client where Client.id=$1;';
       return await client.query(sql, [id]);
 }
 async function deleteProductById(id){
       const client = await connect();
-      const sql = 'DELETE FROM Product where Product.name=?;';
+      const sql = 'DELETE FROM Product where Product.name=$1;';
       return await client.query(sql, [id]);
 }
 async function deletePurchasesById(id){
       const client = await connect();
-      const sql = 'DELETE FROM Purchases where Purchase.idProduct=?;';
+      const sql = 'DELETE FROM Purchases where Purchase.idProduct=$1;';
       return await client.query(sql, [id]);
+}
+
+
+async function selectPurchasesByClient(id){
+      const client = await connect();
+      const sql = 'SELECT * FROM purchases WHERE purchases.idclient=$1;';
+      return await client.query(sql, [id]);
+}
+
+async function selectPurchasesByClientandDate(id,type,data){
+  var typedata='';
+  switch (type) {
+                  case 'day':
+                  typedata='DD-MM-YYYY';
+                  break;
+                  case 'month':
+                  typedata='MM-YYYY';
+                  break;
+                  case 'year':
+                  typedata='YYYY';
+                  break;
+                  default:
+                  typedata='DD-MM-YYYY';
+                }
+      console.log(typedata)
+      const client = await connect();
+      const sql = 'SELECT * from purchases WHERE purchases.idclient=$1  AND  to_char(DATE, $2)= to_char(date($3) , $2);';
+      const rows =  await client.query(sql,[id,typedata,data]);
+      return rows.rows;
 }
 
 
@@ -93,7 +122,4 @@ async function deletePurchasesById(id){
 
 
 
-
-
-
-module.exports = {selectClient,selectProduct,selectPurchases,insertClient,insertProduct,insertPurchases,updateClientById,updateProductById,updatePurchasesById,deleteClientById,deleteProductById,deletePurchasesById}
+module.exports = {selectClient,selectProduct,selectPurchases,insertClient,insertProduct,insertPurchases,updateClientById,updateProductById,updatePurchasesById,deleteClientById,deleteProductById,deletePurchasesById,selectPurchasesByClientandDate}
